25b6749457163a78dedad7bfc5d9f1f6eae9f31d
Jon Chan <jon.chan@parachutehealth.com>
2018-11-14 23:38:00 -0500

    change password to env variable

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,9 @@
 class SessionsController < ApplicationController
-  PASSWORD = "chocolateasdfhjkaskhjfkjsaf"
-
   def new
   end
 
   def create
-    if params[:session][:password] == PASSWORD
+    if params[:session][:password] == ENV["PASSWORD"]
       enter(request.remote_ip)
       redirect_to root_path
     else
